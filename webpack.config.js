2ee7ebb52511aab07b82e53d61af6c7f1b921a07
Madhava Hansen <madhavah@gmail.com>
2020-09-27 16:17:16 -0400

    clean up JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
 
 module.exports = {
   entry: "./lib/main.js",
   output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: "bundle.js"
-	},
-  devtool: "source-map"
+    path: path.resolve(__dirname, "lib"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: "babel-loader" },
+      { exclude: /node_modules/ },
+    ],
+  },
+  devtool: "source-map",
 };
