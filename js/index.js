8f3cf73960553166d488435c4b23640360ce5e70
Madhava Hansen <madhavah@gmail.com>
2023-09-29 15:52:29 -0600

    delete old website and push new version

---
+++ js/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const renderStars = () => {
+    const numStars = 600;
+    const space = document.querySelector(".Space");
+    for (let i = 0; i < numStars; i++) {
+      const star = document.createElement("div");
+      // generate random sized stars to look more realistic
+      if (i % 10 === 0) {
+        star.classList.add("Star");
+      } else if (i % 3 === 0) {
+        star.classList.add("StarWithoutFlicker");
+      } else if (i % 34 === 0) {
+        star.classList.add("StarWithAlternateSize");
+      } else {
+        star.classList.add("TinyStar");
+      }
+      star.style.left = `${Math.random() * 100}%`;
+      star.style.top = `${Math.random() * 100}%`;
+      space.appendChild(star);
+    }
+  }
+  const setMyWorkButtonEventListener = () => {
+    const yellowButton = document.querySelector(".Homepage-myWorkButtonWrapper");
+    const projects = document.querySelector(".Projects");
+    yellowButton.addEventListener("click", () => {
+      projects.scrollIntoView({behavior: 'smooth'});
+    })
+  }
+
+  renderStars()
+  setMyWorkButtonEventListener()
+})
+
+
